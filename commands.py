40c94144f74d44509434f2aeaa677d9836d1c71b
Charles Talbot <ctalbot@student.unimelb.edu.au>
2016-11-18 19:50:18 +1100

    Refurbished the command system.
    Changes the command system so that the main goopbot.py module doesn't have to call each command. Made it so commands.py will handle all of the commands.

--- commands.py
+++ commands.py
@@ -1,48 +1,70 @@
 import discord
 import asyncio
 
-# Takes in the command !commands and returns a list of
-# the commands thar are available from a file called 'commandlist'
-async def commandhelp(client, message):
-    command_file = open('commandlist', 'r')
-    command_list = []
-    for line in command_file:
-        command_list.append(line.rstrip())
-    await client.send_message(message.channel, ', '.join(command_list))
-    command_file.close()
+class Commands:
+    # Checks to see if the command is within the list and if it is
+    # then executes it. !help and !commands both have to be taken
+    # care of seperately due to having function names that are
+    # different to their command names.
+    async def checkcommands(self, client, message):
+        command_list = self.get_commands()
+        in_command = message.content.split(' ')[0].replace('!', '')
+        if in_command == 'help':
+            await self.showhelp(client, message)
+        elif in_command == 'commands':
+            await self.commandhelp(client, message)
+        else:
+            if hasattr(self, in_command):
+                cmd = getattr(self, in_command)
+                await cmd(client, message)
 
-# Takes the !woof command and barks.
-async def woof(client, message):
-    await client.send_message(message.channel, 'bark')
+    # Takes in the command !commands and returns a list of
+    # the commands thar are available from a file called 'commandlist'
+    async def commandhelp(self, client, message):
+        command_list = self.get_commands()
+        await client.send_message(message.channel, ', '.join(command_list))
 
-# Takes the !loveme command and shares the love.
-async def loveme(client, message):
-    await client.send_message(message.channel, ':heart:')
+    # Gets the list of commands within the 'commandlist' file.
+    def get_commands(self):
+        command_file = open('commandlist', 'r')
+        command_list = []
+        for line in command_file:
+            command_list.append(line.rstrip())
+        command_file.close()
+        return command_list
 
-# Takes the !help command and warns that the sender is distressed.
-async def showhelp(client, message):
-    sender =  message.author.nick
-    if sender is None:
-        sender = message.author.name
-    await client.send_message(message.channel, 'plz help ' + sender)
-    await client.send_message(message.channel, 'they in distress')
+    # Takes the !woof command and barks.
+    async def woof(self, client, message):
+        await client.send_message(message.channel, 'bark')
 
-# Takes the !git command and returns the URL for the repo.
-async def git(client, message):
-    url = 'https://github.com/CharlesTalbot123/discord-goopbot'
-    await client.send_message(message.channel, url)
+    # Takes the !loveme command and shares the love.
+    async def loveme(self, client, message):
+        await client.send_message(message.channel, ':heart:')
 
-# Takes the !bigwoof command and does a big bark.
-async def bigwoof(client, message):
-    await client.send_message(message.channel, '**BARK**')
+    # Takes the !help command and warns that the sender is distressed.
+    async def showhelp(self, client, message):
+        sender =  message.author.nick
+        if sender is None:
+            sender = message.author.name
+        await client.send_message(message.channel, 'plz help ' + sender)
+        await client.send_message(message.channel, 'they in distress')
 
-# Takes the !woofwoof command and does a couple of barks.
-async def woofwoof(client, message):
-    await client.send_message(message.channel, 'bark bark')
+    # Takes the !git command and returns the URL for the repo.
+    async def git(self, client, message):
+        url = 'https://github.com/CharlesTalbot123/discord-goopbot'
+        await client.send_message(message.channel, url)
 
-# Takes the !hug command and gives the sender a well needed hug.
-async def hug(client, message):
-    sender = message.author.nick
-    if sender is None:
-        sender = message.author.name
-    await client.send_message(message.channel, '*doghugs for ' + sender + '*')
+    # Takes the !bigwoof command and does a big bark.
+    async def bigwoof(self, client, message):
+        await client.send_message(message.channel, '**BARK**')
+
+    # Takes the !woofwoof command and does a couple of barks.
+    async def woofwoof(self, client, message):
+        await client.send_message(message.channel, 'bark bark')
+
+    # Takes the !hug command and gives the sender a well needed hug.
+    async def hug(self, client, message):
+        sender = message.author.nick
+        if sender is None:
+            sender = message.author.name
+        await client.send_message(message.channel, '*doghugs for ' + sender + '*')
--- goopbot.py
+++ goopbot.py
@@ -6,6 +6,7 @@ token_file = open('token', 'r')
 token = token_file.read()
 
 client = discord.Client()
+commands = Commands()
 
 @client.event
 async def on_ready():
@@ -16,31 +17,8 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('!commands'):
-        print('Received !commands from ' + message.author.name)
-        await commandhelp(client, message)
-    elif message.content.startswith('!loveme'):
-        print('Received !loveme from ' + message.author.name)
-        await loveme(client, message)
-    elif message.content.startswith('!help'):
-        print('Received !help from ' + message.author.name)
-        await showhelp(client, message)
-    elif message.content.startswith('!git'):
-        print('Received !git from ' + message.author.name)
-        await git(client, message)
-    elif message.content.startswith('!bigwoof'):
-        print('Reveived !bigwoof from ' + message.author.name)
-        await bigwoof(client, message)
-    elif message.content.startswith('!woofwoof'):
-        print('Reveived !woofwoof from ' + message.author.name)
-        await woofwoof(client, message)
-    elif message.content.startswith('!woof'):
-        print('Received !woof from ' + message.author.name)
-        await woof(client, message)
-    elif message.content.startswith('!hug'):
-        print('Received !hug from ' + message.author.name)
-        await hug(client, message)
-    elif 'kill' in message.content.lower() and 'goopbot' in message.content.lower():
+    await commands.checkcommands(client, message)
+    if 'kill' in message.content.lower() and 'goopbot' in message.content.lower():
         # Please don't hurt goopbot. He's a good boy.
         await client.send_message(message.channel, '*frighten*')
     elif 'love' in message.content.lower() and 'goopbot' in message.content.lower():
